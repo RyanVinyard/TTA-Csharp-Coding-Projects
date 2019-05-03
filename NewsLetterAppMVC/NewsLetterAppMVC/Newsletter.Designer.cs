76d9fca4a27601502412d01a85a8b8d5b0191104
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-05-02 17:53:37 -0700

    update entity framework

--- NewsLetterAppMVC/NewsLetterAppMVC/Newsletter.Designer.cs
+++ NewsLetterAppMVC/NewsLetterAppMVC/Newsletter.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ravry\source\repos\TTA-Csharp-Coding-Projects\TTA-Csharp-Coding-Projects\NewsLetterAppMVC\NewsLetterAppMVC\Newsletter.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Ryan\Documents\GitHub\TTA-Csharp-Coding-Projects\NewsLetterAppMVC\NewsLetterAppMVC\Newsletter.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
