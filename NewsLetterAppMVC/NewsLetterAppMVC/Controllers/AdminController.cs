0694de8640de64942f0a187c44a967aa44bb9553
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-05-03 02:37:21 -0700

    Add basic unsubscribe functionality

---
+++ NewsLetterAppMVC/NewsLetterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using NewsLetterAppMVC.Models;
+using NewsLetterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsLetterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                var signups = db.SignUps;
+                var signupVms = new List<SignupVm>();
+                foreach (var signup in signups)
+                {
+                    var signupVm = new SignupVm();
+                    signupVm.Id = signup.Id;
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+
+                return View(signupVms);
+            }
+        }
+    }
+}
--- NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
+++ NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
@@ -13,8 +13,7 @@ namespace NewsletterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
+       
         public ActionResult Index()
         {
             return View();
@@ -41,48 +40,12 @@ namespace NewsletterAppMVC.Controllers
                     db.SaveChanges();
                 }
 
-                    //string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES (@FirstName, @LastName, @EmailAddress)";
 
-                    //using (SqlConnection connection = new SqlConnection(connectionString))
-                    //{
-                    //    SqlCommand command = new SqlCommand(queryString, connection);
-                    //    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    //    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    //    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
-
-                    //    command.Parameters["@FirstName"].Value = firstName;
-                    //    command.Parameters["@LastName"].Value = lastName;
-                    //    command.Parameters["@EmailAddress"].Value = emailAddress;
-
-                    //    connection.Open();
-                    //    command.ExecuteNonQuery();
-                    //    connection.Close();
-                    //}
                     return View("Success");
             }
         }
 
-        public ActionResult Admin()
-        {
 
-            using (NewsletterEntities db = new NewsletterEntities())
-            {
-                var signups = db.SignUps;
-                var signupVms = new List<SignupVm>();
-                foreach (var signup in signups)
-                {
-                    var signupVm = new SignupVm();
-                    signupVm.FirstName = signup.FirstName;
-                    signupVm.LastName = signup.LastName;
-                    signupVm.EmailAddress = signup.EmailAddress;
-                    signupVms.Add(signupVm);
-                }
-
-                return View(signupVms);
-            }
-
-            
-        }
 
     }
 }
